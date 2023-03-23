d45bef377edddce8775cff0cbbb299510653cdea
DavidPS <david.pujol.santander@gmail.com>
2023-03-23 19:13:40 +0100

    Scripts_Alpha

---
+++ Ramen_EscapaDeLaJaula_/Assets/Scripts/CamMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CamMovement : MonoBehaviour
+{
+    public GameObject Camera;
+    private Vector2 LookPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        LookAround();
+    }
+    void OnLook(InputValue value)
+    {
+        LookPos = value.Get<Vector2>();
+    }
+    void LookAround()
+    {
+        Camera.transform.RotateAround(transform.position, Vector3.up, LookPos.x);
+        Camera.transform.LookAt(transform.position);
+    }
+}
---
+++ Ramen_EscapaDeLaJaula_/Assets/Scripts/MonedaInteraction.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonedaInteraction : MonoBehaviour
+{
+    public int contador = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(CharacterController.)
+    }
+}
