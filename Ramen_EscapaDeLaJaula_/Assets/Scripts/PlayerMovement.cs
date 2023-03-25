621a50200e446355d20e6c4e76f2b658002fbe23
Davidps9 <david.pujol.santander@gmail.com>
2023-03-25 11:31:50 +0100

    xd

--- Ramen_EscapaDeLaJaula_/Assets/Scripts/PlayerMovement.cs
+++ Ramen_EscapaDeLaJaula_/Assets/Scripts/PlayerMovement.cs
@@ -8,17 +8,19 @@ public class PlayerMovement : MonoBehaviour
 
     public float speedDivider = 5f;
     private Vector2 raw_movement;
-
+    private CharacterController Cc;
+    public float gravity = -9.8f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cc = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        
     }
 
     void OnMove(InputValue value)
@@ -28,6 +30,24 @@ public class PlayerMovement : MonoBehaviour
 
     void Movement()
     {
+
         transform.Translate(raw_movement.x / speedDivider, 0, raw_movement.y / speedDivider);
+        if (!Cc.isGrounded)
+        {
+            //ApplyGravity();
+            Debug.Log("Volan2");
+
+        }
+        else
+        {
+            Debug.Log("Hola");
+        }
+
+    }
+    void ApplyGravity()
+    {
+
+            transform.Translate(0,gravity,0);
+        
     }
 }
