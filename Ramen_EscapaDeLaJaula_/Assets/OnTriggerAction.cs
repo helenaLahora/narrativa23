22f89696ddb09c5fb32d60ba6f84cde658c129f8
DavidPS <david.pujol.santander@gmail.com>
2023-05-11 19:43:12 +0200

    muchos cambios

--- Ramen_EscapaDeLaJaula_/Assets/OnTriggerAction.cs
+++ Ramen_EscapaDeLaJaula_/Assets/OnTriggerAction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class OnTriggerAction : MonoBehaviour
 {
     private bool playerIn = false;
+    private bool rotated = false;
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -24,9 +25,15 @@ public class OnTriggerAction : MonoBehaviour
 
     private void OnInteract()
     {
-        if (playerIn)
+        if (playerIn && !rotated)
         {
             transform.Rotate(new Vector3(0, 90, 0));
+            rotated = true;
+        }
+        else if (rotated && playerIn)
+        {
+            transform.Rotate(new Vector3(0, -90, 0));
+            rotated = false;
         }
     }
 
