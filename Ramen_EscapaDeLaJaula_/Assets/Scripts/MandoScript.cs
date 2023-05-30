d1415398cfc6cd831281916b2a766e0f223b584f
Davidps9 <david.pujol.santander@gmail.com>
2023-05-30 11:03:11 +0200

    mando por input

--- Ramen_EscapaDeLaJaula_/Assets/Scripts/MandoScript.cs
+++ Ramen_EscapaDeLaJaula_/Assets/Scripts/MandoScript.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MandoScript : MonoBehaviour
 {
     [SerializeField] private TeleChange teleChange;
+    private bool interacted = false;
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && interacted)
         {
             teleChange.ResetMat();
+            interacted = false;
         }
+    }
+    public void OnInteract()
+    {
+        interacted = true;
     }
 }
