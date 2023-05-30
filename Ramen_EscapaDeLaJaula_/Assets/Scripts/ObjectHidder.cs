0fedf20241fb6a20e0d45e3a3782c5b446e20c48
Davidps9 <david.pujol.santander@gmail.com>
2023-05-30 12:05:35 +0200

    creo que la salchicha funsiona

---
+++ Ramen_EscapaDeLaJaula_/Assets/Scripts/ObjectHidder.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectHidder : MonoBehaviour
+{
+    [SerializeField] private Variable variable;
+    [SerializeField] private float valorAComparar;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (EventHandler.Variables[variable] > valorAComparar)
+        {
+            Hide();
+        }
+    }
+    public void Hide()
+    {       
+            gameObject.SetActive(false);                
+    }
+}
